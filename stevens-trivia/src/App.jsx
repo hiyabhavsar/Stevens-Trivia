8cf8e4143c3fa22a6eefb34133de22ad3ee07489
hiyabhavsar <hiyabhavsar@gmail.com>
2024-04-05 18:54:38 -0400

    start page is styled, navigation fixed

--- stevens-trivia/src/App.jsx
+++ stevens-trivia/src/App.jsx
@@ -1,11 +1,30 @@
-import "./App.css";
+import "./css/App.css";
 import Quiz from "./Quiz";
 import StartPage from "./startPage";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
 import { jsQuizz } from "./constants";
 
 function App() {
-  return;
-  <Quiz questions={jsQuizz.questions} />;
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route
+            path="/Quiz"
+            element={<Quiz questions={jsQuizz.questions} />}
+          />
+        </Routes>
+      </Router>
+      {/* <Quiz questions={jsQuizz.questions} /> */}
+    </>
+  );
 }
 
 export default App;
--- stevens-trivia/src/startPage.jsx
+++ stevens-trivia/src/startPage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import "./css/startPage.scss";
 
 function StartPage() {
   let navigate = useNavigate();
