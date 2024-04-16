ea2824a7b107d40d06ebc1ecd38a9e3ba5704fec
hiyabhavsar <hiyabhavsar@gmail.com>
2024-04-16 16:50:13 -0400

    uploaded questions

--- stevens-trivia/src/constants.js
+++ stevens-trivia/src/constants.js
@@ -1,56 +1,290 @@
 export const jsQuizz = {
   questions: [
     {
+      //Question 1
       question:
-        "Which of the following is used in React.js to increase performance?",
+        "What architectural feature distinguished the Howe Building on Stevens campus upon its completion in 1911?",
       choices: [
-        "Virtual DOM",
-        "Original DOM",
-        "Both A and B",
-        "None of the above",
+        "A Rooftop Garden",
+        "A Giant Clock Tower",
+        "A Heated Indoor Pool",
+        "A Floating Staircase",
       ],
       type: "MCQs",
-      correctAnswer: "Virtual DOM",
-      explanation: "q1 explanation",
+      correctAnswer: "A Floating Staircase",
+      explanation:
+        "Fun Fact: The Howe Building featured one of only two 'floating staircases' in America at the time, showcasing an unsupported cantilevered staircase with an intricately hand-carved balustrade, providing students and visitors with a breathtaking architectural experience.",
     },
     {
-      question: "What is ReactJS?",
-      choices: [
-        "Server-side framework",
-        "User Interface framework",
-        "both a and b",
-        "None of the above",
-      ],
-      type: "MCQs",
-      correctAnswer: "User Interface framework",
-      explanation: "q2 explanation",
-    },
-    {
+      //Question 2
       question:
-        "Identify the one which is used to pass data to components from outside",
-      choices: ["Render with arguments", "setState", "PropTypes", "props"],
-      type: "MCQs",
-      correctAnswer: "props",
-      explanation: "q3 explanation",
-    },
-    {
-      question: "In which language is React.js written?",
-      choices: ["Python", "Java", "C#", "JavaScript"],
-      type: "MCQs",
-      correctAnswer: "JavaScript",
-      explanation: "q4 explanation",
-    },
-    {
-      question: "What is Babel?",
+        "What nickname has Stevens Institute of Technology earned due to its challenging academic programs?",
       choices: [
-        "JavaScript interpreter",
-        "JavaScript transpiler",
-        "JavaScript compiler",
-        "None of the above",
+        "The Castle on the Hudson",
+        "The School of Hard Knocks",
+        "The Inventors' Workshop",
+        "The Big Apple's Brainiacs",
       ],
       type: "MCQs",
-      correctAnswer: "JavaScript compiler",
-      explanation: "q5 explanation",
+      correctAnswer: "The School of Hard Knocks",
+      explanation:
+        "Fun Fact: Stevens gained the moniker 'The School of Hard Knocks' as a testament to the rigorous academic curriculum that prepares students for successful careers in engineering and technology.",
+    },
+    {
+      //Question 3
+      question:
+        "Which esteemed scientist once served on the faculty at Stevens?",
+      choices: [
+        "Albert Einstein",
+        "Marie Curie",
+        "Irving Langmuir",
+        "Stephen Hawking",
+      ],
+      type: "MCQs",
+      correctAnswer: "Irving Langmuir",
+      explanation:
+        "Fun Fact: Irving Langmuir, a Nobel laureate in Chemistry, graced the faculty at Stevens from 1906 to 1909, leaving a lasting impact on various scientific disciplines with his groundbreaking research in surface chemistry.",
+    },
+    {
+      //Question 4
+      question:
+        "When did Stevens Institute of Technology first admit women into its graduate program?",
+      choices: ["1890s", "1950s", "1960s", "1970s"],
+      type: "MCQs",
+      correctAnswer: "1970s",
+      explanation:
+        "Fun Fact: Although Stevens welcomed female graduate students in 1967, undergraduate women were not admitted until 1971, marking a significant milestone in the university's history of inclusivity.",
+    },
+    {
+      //Question 5
+      question:
+        "What innovative requirement did Stevens Institute of Technology become the first university in the US to implement?",
+      choices: [
+        "Mandatory gym classes",
+        "On-campus housing for all students",
+        "Personal computers for all freshmen",
+        "A foreign language requirement",
+      ],
+      type: "MCQs",
+      correctAnswer: "Personal computers for all freshmen",
+      explanation:
+        "Fun Fact: In 1982, Stevens became a pioneer by mandating that all incoming freshmen own and utilize a personal computer, pioneering the integration of technology into higher education.",
+    },
+    {
+      //Question 6
+      question:
+        "What are the two national Centers of Excellence housed at Stevens?",
+      choices: [
+        "Robotics and Aerospace Engineering",
+        "Cybersecurity and Marine Technology",
+        "Chemical Engineering and Sustainable Energy",
+        "Biomedical Engineering and Artificial Intelligence",
+      ],
+      type: "MCQs",
+      correctAnswer: "Cybersecurity and Marine Technology",
+      explanation:
+        "Fun Fact: The U.S. Department of Defense and Homeland Security recognize Stevens for its Centers of Excellence in Cybersecurity and Marine Technology, showcasing the university's cutting-edge research focus.",
+    },
+    {
+      //Question 7
+      question:
+        "What legendary figure in American history once visited Stevens?",
+      choices: [
+        "Abraham Lincoln",
+        "Theodore Roosevelt",
+        "Thomas Edison",
+        "Albert Einstein",
+      ],
+      type: "MCQs",
+      correctAnswer: "Theodore Roosevelt",
+      explanation:
+        "Fun Fact: President Theodore Roosevelt, known for his adventurous spirit and emphasis on technological advancements, visited the Stevens campus in 1907.",
+    },
+    {
+      //Question 8
+      question:
+        "What event at Stevens Institute of Technology symbolizes student unity and support for innovation?",
+      choices: [
+        "The Pumpkin Smash (a yearly pumpkin carving contest)",
+        "The Hackathon (a marathon coding competition)",
+        "The Innovation Expo (a showcase of student projects and startups)",
+        "The Color Run (a colorful charity fun run)",
+      ],
+      type: "MCQs",
+      correctAnswer:
+        "The Innovation Expo (a showcase of student projects and startups)",
+      explanation:
+        "Fun Fact: The Innovation Expo at Stevens Institute of Technology is a vibrant event that celebrates student creativity and entrepreneurial spirit, providing a platform for showcasing groundbreaking projects and startups.",
+    },
+    {
+      //Question 9
+      question:
+        "What recent initiative at Stevens Institute of Technology promotes sustainability and environmental consciousness?",
+      choices: [
+        "The Green Building Challenge (a competition to design eco-friendly structures)",
+        "The Zero-Waste Week (a campus-wide campaign to reduce waste)",
+        "The Solar Panel Installation (a project to power campus buildings with renewable energy)",
+        "The Bike Share Program (a service offering bicycles for campus transportation)",
+      ],
+      type: "MCQs",
+      correctAnswer:
+        "The Zero-Waste Week (a campus-wide campaign to reduce waste)",
+      explanation:
+        "Fun Fact: Stevens Institute of Technology's Zero-Waste Week encourages sustainable practices such as recycling, composting, and reducing single-use plastics, fostering a greener campus environment",
+    },
+    {
+      //Question 10
+      question:
+        "Which cultural event at Stevens Institute of Technology celebrates diversity and multiculturalism?",
+      choices: [
+        "International Food Festival (a showcase of global cuisine)",
+        "Global Awareness Week (a series of educational workshops on world issues)",
+        "International Fashion Show (a display of traditional attire from different countries)",
+        "Annual Unity Showcase (a performance extravaganza of music and dance)",
+      ],
+      type: "MCQs",
+      correctAnswer:
+        "Annual Unity Showcase (a performance extravaganza of music and dance)",
+      explanation:
+        "Fun Fact: Annual Unity Showcase and Carnival is an event where multicultural organizations participate in this annual performance to highlight and celebrate their rich diversity and culture for the entire Stevens community.",
+    },
+    {
+      //Question 11
+      question:
+        "What unique feature of Stevens Institute of Technology's campus reflects its commitment to innovation and collaboration?",
+      choices: [
+        "The Idea Incubator (a space for entrepreneurial brainstorming)",
+        "The Innovation Lab (a facility for prototyping and experimentation)",
+        "The Stevens Greenhouse (a research facility for sustainable agriculture)",
+        "The MakerSpace (a workshop for hands-on engineering projects)",
+      ],
+      type: "MCQs",
+      correctAnswer:
+        "The Stevens Greenhouse (a research facility for sustainable agriculture)",
+      explanation:
+        "Fun Fact: The Stevens Greenhouse serves as a hub for interdisciplinary research and experimentation, providing students and faculty with opportunities to explore innovative solutions to environmental challenges.",
+    },
+    {
+      //Question 12
+      question:
+        "Which pioneering aspect of transportation was tested on Stevens property in the 19th century?",
+      choices: [
+        "Steam-powered locomotives",
+        "Early Ferris wheels",
+        "Modern subway systems",
+        "Demonstration locomotives on circular tracks",
+      ],
+      type: "MCQs",
+      correctAnswer: "Demonstration locomotives on circular tracks",
+      explanation:
+        "Fun Fact: Stevens Institute of Technology was a testing ground for demonstration locomotives on circular tracks, showcasing innovative transportation technologies long before modern subway systems were developed.",
+    },
+    {
+      //Question 13
+      question:
+        "What significant role did Stevens Institute of Technology play in the development of baseball?",
+      choices: [
+        "Inventing the rules of modern baseball",
+        "Hosting the first recorded modern baseball game",
+        "Constructing the first baseball stadium",
+        "Training professional baseball players",
+      ],
+      type: "MCQs",
+      correctAnswer: "Hosting the first recorded modern baseball game",
+      explanation:
+        "Fun Fact: Stevens Institute of Technology hosted the first recorded modern baseball game on its grounds, where the Knickerbocker Club faced off against the New York Nine, marking a historic moment in the evolution of America's national pastime.",
+    },
+    {
+      //Question 14
+      question:
+        "Which engineering discipline was the first to be taught at Stevens Institute of Technology?",
+      choices: [
+        "Mechanical Engineering",
+        "Civil Engineering",
+        "Electrical Engineering",
+        "Chemical Engineering",
+      ],
+      type: "MCQs",
+      correctAnswer: "Electrical Engineering",
+      explanation:
+        "Fun Fact: Electrical Engineering was the first engineering discipline to be taught at Stevens Institute of Technology, reflecting the institution's early focus on emerging technologies and innovation.",
+    },
+    {
+      //Question 15
+      question:
+        "Which building on the Stevens campus is named after a former president of the university who made significant contributions to its growth and development?",
+      choices: [
+        "Morton-Peirce-Kidde Complex",
+        "Burchard Building",
+        "Jonas Hall",
+        "Lawrence T. Babbio Jr. Center for Technology Management",
+      ],
+      type: "MCQs",
+      correctAnswer: " Lawrence T. Babbio Jr. Center for Technology Management",
+      explanation:
+        "Fun Fact: The Lawrence T. Babbio Jr. Center for Technology Management is named after a former president of Stevens Institute of Technology who played a key role in the university's growth and development, particularly in advancing technology management education.",
+    },
+    {
+      //Question 16
+      question:
+        "In what year did Stevens Institute of Technology move its campus to its current location in Hoboken, New Jersey?",
+      choices: ["1820", "1870", "1871", "1900"],
+      type: "MCQs",
+      correctAnswer: "1871",
+      explanation:
+        "Fun Fact: Stevens Institute of Technology moved its campus to its current location in Hoboken, New Jersey, in 1871, marking a significant milestone in the university's history and expansion.",
+    },
+    {
+      //Question 17 **
+      question: "Question",
+      choices: [
+        "Robotics and Aerospace Engineering",
+        "Cybersecurity and Marine Technology",
+        "Chemical Engineering and Sustainable Energy",
+        "Biomedical Engineering and Artificial Intelligence",
+      ],
+      type: "MCQs",
+      correctAnswer: "Cybersecurity and Marine Technology",
+      explanation: "Fun Fact:",
+    },
+    {
+      //Question 18
+      question: "Question",
+      choices: [
+        "Robotics and Aerospace Engineering",
+        "Cybersecurity and Marine Technology",
+        "Chemical Engineering and Sustainable Energy",
+        "Biomedical Engineering and Artificial Intelligence",
+      ],
+      type: "MCQs",
+      correctAnswer: "Cybersecurity and Marine Technology",
+      explanation: "Fun Fact:",
+    },
+    {
+      //Question 19
+      question: "Question",
+      choices: [
+        "Robotics and Aerospace Engineering",
+        "Cybersecurity and Marine Technology",
+        "Chemical Engineering and Sustainable Energy",
+        "Biomedical Engineering and Artificial Intelligence",
+      ],
+      type: "MCQs",
+      correctAnswer: "Cybersecurity and Marine Technology",
+      explanation: "Fun Fact:",
+    },
+    {
+      //Question 20
+      question: "Question",
+      choices: [
+        "Robotics and Aerospace Engineering",
+        "Cybersecurity and Marine Technology",
+        "Chemical Engineering and Sustainable Energy",
+        "Biomedical Engineering and Artificial Intelligence",
+      ],
+      type: "MCQs",
+      correctAnswer: "Cybersecurity and Marine Technology",
+      explanation: "Fun Fact:",
     },
   ],
 };
